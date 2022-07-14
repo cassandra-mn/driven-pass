ab0aaf6c2c7fca11f2eadcc197c032553c25791e
cassandra-mn <arthenciac@gmail.com>
2022-07-14 13:47:15 -0300

    chore: add structure basic

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, {json} from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware.js';
+import router from './routers/index.js';
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+app.use(router);
+app.use(errorHandlerMiddleware);
+
+const PORT = +process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
