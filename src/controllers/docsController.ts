43a3064a7a9ee8df2cf6f4fe07db54766550b82f
cassandra-mn <arthenciac@gmail.com>
2022-07-17 12:24:20 -0300

    feat: create docs

--- src/controllers/docsController.ts
+++ src/controllers/docsController.ts
@@ -1,8 +1,13 @@
 import {Request, Response} from 'express';
 
+import {DocsData} from '../utils/docsData.js';
+import * as docsService from '../services/docsService.js';
+
 export async function create(req: Request, res: Response) {
-    
-    res.sendStatus(501);
+    const docs: DocsData = req.body;
+    const {userId} = res.locals;
+    await docsService.create({...docs, userId});
+    res.sendStatus(201);
 }
 
 export async function find(req: Request, res: Response) {
